7b683f5e7586ce6e16ead19f5a6dd5ef89ed6a81
cmatevz <matevz.cerne@gmail.com>
2015-03-18 13:43:22 +0100

    timestamp-php

--- htdocs/car_srv.php
+++ htdocs/car_srv.php
@@ -5,7 +5,7 @@ $cars = array("193.2.178.98:80", "192.168.1.108:12345");
 //$cars = array("193.2.178.98:80", "192.168.1.148:80");
 
 //$request = "http://" . $cars[$_GET["id"]] . "/?command=" . $_GET["command"];
-$request = "http://" . $cars[$_GET["id"]] . "/?up=" . $_GET["up"] . "&down=" . $_GET["down"] . "&left=" . $_GET["left"] . "&right=" . $_GET["right"];
+$request = "http://" . $cars[$_GET["id"]] . "/?up=" . $_GET["up"] . "&down=" . $_GET["down"] . "&left=" . $_GET["left"] . "&right=" . $_GET["right"0] . "&time=" . $_GET["time"];
 $output = file_get_contents($request);
 
 header("Access-Control-Allow-Origin: *");
