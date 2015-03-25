7e570a206fe3c4a47b9d3208eb07195b23a4a579
cmatevz <matevz.cerne@gmail.com>
2015-03-25 17:55:08 +0100

    avtomatski IP v JS

--- htdocs/comm.js
+++ htdocs/comm.js
@@ -10,7 +10,10 @@ var commandCount = 0;
 //var URL = "http://193.2.176.139:80/car_srv.php";
 //var URL = "http://192.168.1.147:80/car_srv.php";
 //var URL = "http://193.2.178.39:80/car_srv.php";
-var URL = "http://localhost:80/car_srv.php";
+//var URL = "http://localhost:80/car_srv.php";
+var URL = "http://" + document.URL.split("/")[2] + "/car_srv.php";
+//var naslov = document.URL.split("/")[2];
+
 
 var send = function(message) {
 	$.ajax({
