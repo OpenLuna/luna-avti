07748c121ccceaf5a84d11f655ce3bd8e95d7408
Ernest Beličič <ernest.belicic@gmail.com>
2015-03-27 09:20:33 +0100

    microtime fix

--- htdocs/car_srv.php
+++ htdocs/car_srv.php
@@ -1,21 +1,19 @@
 <?php
 session_start();
-$start_time = microtime();
-//$cars = array("193.2.178.98:80", "193.2.177.130:12345");
-//$cars = array("193.2.178.98:80", "192.168.1.108:12345");
-//$cars = array("193.2.178.98:80", "192.168.1.148:80");
+$start_time = microtime(true);
 $cars = $_SESSION["cars"];
-
-//$request = "http://" . $cars[$_GET["id"]] . "/?command=" . $_GET["command"];
 $request = "http://" . trim($cars[$_GET["id"]]["ip"]) . ":". trim($cars[$_GET["id"]]["port"]) . "/?up=" . $_GET["up"] . "&down=" . $_GET["down"] . "&left=" . $_GET["left"] . "&right=" . $_GET["right"] . "&time=" . $_GET["time"];
 $output = file_get_contents($request);
 
 header("Access-Control-Allow-Origin: *");
-$ex_time = microtime() - $start_time;
+$ex_time = microtime(true) - $start_time;
+if($ex_time < 0){
+	$file = fopen("logs/t.csv", "a");
+	fwrite($file, $start_time . " " . microtime() . "\n");
+	fclose($file);
+}
 $file = fopen("logs/log.csv", "a");
 $data = array(trim($cars[$_GET["id"]]["name"]), $_GET["time"], "PHP", $ex_time);
 echo fputcsv($file, $data);
 fclose($file);
-//echo $output;
-
 ?>
