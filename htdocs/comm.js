00d95cf3441a4a0eb83a844dc3071b9d1eee08a7
Ernest Beličič <ernest.belicic@gmail.com>
2015-04-15 23:49:28 +0200

    logiranje casa requesta

--- htdocs/comm.js
+++ htdocs/comm.js
@@ -8,6 +8,9 @@ var OFF = "OFF";
 var SERVER_URL = "http://" + document.URL.split("/")[2] + "/car_srv.php";
 var websocket = null;
 
+timeSum = 0; //logiranje
+packetCnt = 0; //logiranje
+
 function wsConnect(){
 	if(websocket != null){
 		websocket.close();
@@ -17,13 +20,17 @@ function wsConnect(){
 		websocket = new WebSocket(carURL);
 		websocket.onopen = function(evt){
 			$("button#wsconnect").text("Disconnect");
+			timeSum = 0; //logiranje
+			packetCnt = 0; //logiranje
 		};
 		websocket.onclose = function(evt){
 			$("button#wsconnect").text("Connect");
 			websocket = null;
+			console.log("Average: " + (timeSum / packetCnt) + " ms"); //logiranje
 		};
 		websocket.onmessage = function(evt){
-			console.log(evt);
+			timeSum += (new Date()).getTime() - parseInt(evt.data); //logiranje
+			console.log((new Date()).getTime() - parseInt(evt.data)); //logiranje
 		};
 		websocket.onerror = function(evt){
 			console.log(evt);
@@ -34,14 +41,15 @@ function wsConnect(){
 function sendState(){
 	if(websocket == null) return;
 	
+	var timestamp = (new Date()).getTime();
 	var left = (keysdown[LEFT] === true) ? ON : OFF;
 	var right = (keysdown[RIGHT] === true) ? ON : OFF;
 	var up = (keysdown[UP] === true) ? ON : OFF;
 	var down = (keysdown[DOWN] === true) ? ON : OFF;
-	var timestamp = (new Date()).getTime();
 	
 	var cmd = "?up=" + up + "&down=" + down + "&left=" + left + "&right=" + right + "&time=" + timestamp;
 	websocket.send(cmd);
+	packetCnt++; //logiranje
 }
 
 function keypressHandle(e){
