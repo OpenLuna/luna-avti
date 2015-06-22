c58d1b0247139a94fbb84af7420a83eeb285f32d
Ernest Beličič <ernest.belicic@gmail.com>
2015-06-22 04:00:41 +0200

    cars list populated over ajax

--- bridge server/public/scripts/main.js
+++ bridge server/public/scripts/main.js
@@ -4,23 +4,49 @@ var UP = 73; //i
 var DOWN = 75; //k
 var ON = "ON";
 var OFF = "OFF";
-var WS_SERVER = "ws://" + window.location.href.split("/")[2].split(":")[0] + ":4113/";
+var WS_URL = "ws://" + window.location.href.split("/")[2].split(":")[0] + ":4113/";
+var HTTP_URL = "http://" + window.location.href.split("/")[2].split(":")[0] + ":80/"
 
 var keysdown = {};
 var websocket = null;
+
+function keypressHandle(e){
+	if(e.type == "keydown"){
+		if(!keysdown[e.keyCode]){
+			keysdown[e.keyCode] = true;
+			sendState();
+		}
+	}
+	else if(e.type == "keyup"){
+		keysdown[e.keyCode] = false;
+		sendState();
+	}
+	else console.error(e);
+}
+
+function sendState(){
+	if(websocket != null){
+		var left = (keysdown[LEFT] === true) ? ON : OFF;
+		var right = (keysdown[RIGHT] === true) ? ON : OFF;
+		var up = (keysdown[UP] === true) ? ON : OFF;
+		var down = (keysdown[DOWN] === true) ? ON : OFF;
+		
+		var cmd = "up=" + up + "&down=" + down + "&left=" + left + "&right=" + right;
+		websocket.send(cmd);
+	}
+}
 
 function wsConnection(){
 	if(websocket != null){
 		websocket.close();
 	}
 	else{
-		var query = "token=123&name=RangeRover";
-		websocket = new WebSocket(WS_SERVER);
+		var query = "token=123&name=" + encodeURIComponent($("select#cars_list").val());
+		websocket = new WebSocket(WS_URL);
 		
 		websocket.onopen = function(evt){
 			$("button#ws_connect").text("Disconnect");
-			//websocket.send(query);
-			websocket.send("123");
+			websocket.send(query);
 			//$("#camera").attr("src", "http://" + $("#cars_list option:selected").val() + ":80/file.mjpg");
 		};
 		
@@ -40,34 +66,25 @@ function wsConnection(){
 	}
 }
 
-function sendState(){
-	if(websocket != null){
-		var timestamp = (new Date()).getTime();
-		var left = (keysdown[LEFT] === true) ? ON : OFF;
-		var right = (keysdown[RIGHT] === true) ? ON : OFF;
-		var up = (keysdown[UP] === true) ? ON : OFF;
-		var down = (keysdown[DOWN] === true) ? ON : OFF;
-		
-		var cmd = "?up=" + up + "&down=" + down + "&left=" + left + "&right=" + right + "&time=" + timestamp;
-		websocket.send(cmd);
-	}
-}
-
-function keypressHandle(e){
-	if(e.type == "keydown"){
-		if(!keysdown[e.keyCode]){
-			keysdown[e.keyCode] = true;
-			sendState();
+function refreshCarsList(){
+	var URL = HTTP_URL + "cars_list";
+	$("select#cars_list").empty();
+	$.ajax({
+		url: URL,
+		success: function(data, textStatus, jqXHR){
+			data.forEach(function(d){
+				$("select#cars_list").append("<option value='" + d + "'>" + d + "</option>");
+			});
+		},
+		error: function(jqXHR, status, error){
+			console.log(status + " " + error);
 		}
-	}
-	else if(e.type == "keyup"){
-		keysdown[e.keyCode] = false;
-		sendState();
-	}
-	else console.error(e);
+	});
 }
 
 $(document).ready(function() {
 	$(window).keydown(keypressHandle).keyup(keypressHandle);
 	$("button#ws_connect").click(wsConnection);
+	$("button#refresh").click(refreshCarsList);
+	refreshCarsList();
 });
--- bridge server/test.js
+++ bridge server/test.js
@@ -7,45 +7,22 @@ var QueryString = require("querystring");
 
 //app is http server that serves static files in /public folder
 var app = Connect();
-//app.use(Logging("combined")); //turn off logging after dev
+app.use(Logging("combined")); //turn off logging after 
+app.use("/cars_list", function(req, res, next){
+	res.writeHead(200, {"Content-Type": "application/json"});
+	res.end(JSON.stringify(Object.keys(cars)));
+});
 app.use(ServeStatic(__dirname + "/public"));
 app.listen(80);
 
 var CAR_SECRET = 133780085; //car token (shared secret)
-var cars = {};
-
-/*function verifyWS(info, callback){
-	var query = URL.parse(info.req.url, true).query;
-	var accept = true;
-	var message = "";
-	
-	try{
-		var token = parseInt(query.token);
-		
-		if(token == CAR_SECRET){
-			console.log("Car token accepted");
-		}
-		else if(token > 0 && token < 1000){ //token validation
-			console.log("Client token " + token + " accepted");
-		}
-		else{
-			throw "invalid token";
-		}
-	}
-	catch(error){
-		accept = false;
-		message = error;
-	}
-	finally{
-		callback(accept, 404, message);
-	}
-}*/
+var cars = {"Range Rover": "", "Formula 1": ""};
 
 //wss is websocket server
 var wss = new WebSocketServer({port: 4113});
 wss.on("connection", function(ws){
 	ws.on("message", function(message, flags){
-		console.log(message);
+		console.log("Message: " + message);
 		if(ws.carWS === undefined){
 			try{
 				var query = QueryString.parse(message);
@@ -57,7 +34,7 @@ wss.on("connection", function(ws){
 				}
 				else if(token > 0 && token < 1000){ //token validation
 					console.log("Client token " + token + " accepted");
-					console.log("Want connection with " + query.name);
+					console.log("Want connection with " + query.name + "\n");
 				}
 				else{
 					throw "invalid token";
