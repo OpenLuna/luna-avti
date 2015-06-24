8d1ecd4ad89ad94ca7c1ec4108a7e302984eacc5
Ernest Belicic <ernest.belicic@gmail.com>
2015-06-24 13:53:17 +0000

    install script for OS dependencies

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt-get update
+apt-get upgrade
+apt-get install -y python-dev
+apt-get install -y python-pip
+pip install twisted
+pip install autobahn
+apt-get install -y ppp
+apt-get install -y wvdial
+apt-get install -y usb-modeswitch
+apt-get install -y gedit
