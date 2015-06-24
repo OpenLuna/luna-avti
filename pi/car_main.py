289df9c47d3e404e3b95da5cbd3881487496b602
Ernest Belicic <ernest.belicic@gmail.com>
2015-06-24 01:24:15 +0000

    file cleanup

--- pi/car_main.py
+++ pi/car_main.py
@@ -1,5 +1,4 @@
 import car_control as cc
-import car_network as cn
 import picamera
 import urlparse
 import socket
@@ -10,6 +9,7 @@ import sys
 import os
 import io
 
+from config_parser import loadConfig, printDict
 from autobahn.twisted.websocket import WebSocketClientProtocol, WebSocketClientFactory
 from twisted.internet import reactor
 from twisted.internet import task
@@ -17,65 +17,14 @@ from multiprocessing import Process
 
 def applyCommands(up, down, left, right):
     #driving
-    if up == "on":
-        control.drive(control.DRIVE_FORWARD)
-    elif down == "on":
-        control.drive(control.DRIVE_BACKWARD)
-    else:
-        control.drive(control.DRIVE_STOP)
+    if up == "on": control.drive(control.DRIVE_FORWARD)
+    elif down == "on": control.drive(control.DRIVE_BACKWARD)
+    else: control.drive(control.DRIVE_STOP)
     #steering
-    if left == "on":
-        control.steer(control.STEER_LEFT)
-    elif right == "on":
-        control.steer(control.STEER_RIGHT)
-    else:
-        control.steer(control.STEER_STOP)
-    
+    if left == "on": control.steer(control.STEER_LEFT)
+    elif right == "on": control.steer(control.STEER_RIGHT)
+    else: control.steer(control.STEER_STOP)
     print "Got state UP:", up, "DOWN:", down, "LEFT:", left, "RIGHT:", right
-
-#read config from file
-def loadConfig(fileName = None):
-    config = {}
-    lineNo = 0
-    if fileName == None:
-        fileName = os.path.dirname(os.path.realpath(__file__)) + "/car.config"
-    
-    with open(fileName) as f:
-        for line in f:
-            lineNo += 1
-            line = line.strip()
-            if len(line) == 0 or line.startswith("#"):
-                continue
-            line = line.split(":")
-            if len(line) != 2:
-                raise Exception("Error reading config on line " + str(lineNo))
-            key = line[0].strip().lower()
-            value = line[1].strip()
-            if not key or not value:
-                raise Exception("Error reading config on line " + str(lineNo))
-            config[key] = value
-    return config
-
-#print any dict in form:
-#header
-#  key1: value1
-#  key2: value2 ...
-def printDict(header, d):
-    print "\n", header
-    for k in d:
-        print "  " + str(k) + ": " + str(d[k])
-    print
-
-#check if car has network connection by getting local IP
-def hasNetworkConnection():
-    if cn.getLocalIP() == "":
-        print "No network connection"
-        control.stopMotors()
-        control.LED("red", True)
-        time.sleep(0.5)
-        control.LED("red", False)
-        return False
-    return True
 
 class WebsocketClient(WebSocketClientProtocol):
     def __init__(self):
@@ -103,8 +52,8 @@ class WebsocketClient(WebSocketClientProtocol):
             control.stopMotors()
             self.pingTask.stop()
             reactor.connectTCP(config["server ip"], int(config["ws port"]), factory)
-        except:
-            pass
+        except Exception as e:
+            print e
     
     def ping(self):
         if self.gotPong:
@@ -150,26 +99,22 @@ def streaming():
         print "Streaming finished", e
         camera.close()
         conn.close()
+        streaming()
 
 #****MAIN****#
 print "Process nicesness", os.nice(-20)
+
+#initialize car control
+control = cc.Control()
 
 #read default config file (car.config) or the one provided as program argument
 config = loadConfig(sys.argv[2]) if len(sys.argv) > 2 else loadConfig()
 printDict("Config:", config)
 
-#initialize car control
-control = cc.Control()
-
-#TODO: check network connection with mobile dongle
-#while not hasNetworkConnection():
-#    time.sleep(1)
-
 #connect websocket
 factory = WebSocketClientFactory(debug = False)
 factory.protocol = WebsocketClient
 reactor.connectTCP(config["server ip"], int(config["ws port"]), factory)
-#task.LoopingCall(hasNetworkConnection).start(1)
 
 runStreaming = True
 if len(sys.argv) > 1:
